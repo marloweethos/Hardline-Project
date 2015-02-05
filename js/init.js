edc593aa38e9725202a059d7f23ac135da8fe0f0
MarloweEthos <marloweethos@gmail.com>
2015-02-04 20:26:42 -0500

    Update 1
    Added form at bottom changed main image and put more content on the
    page.

--- js/init.js
+++ js/init.js
@@ -1,6 +1,6 @@
 /*
-	Directive by HTML5 UP
-	html5up.net | @n33co
+	Battlefield Hardline
+	Directive framework by HTML5 UP
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
